0a87b5bc6fd9936c6c4a691e3571c50107b8748e
urOHerO <rashitov.cool@yandex.ru>
2025-01-31 01:36:22 +0400

    Сделаны контроллеры для постов. Не работает удаление поста

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,7 @@
 import UserController from './user-controller'
+import PostController from './post-controller'
 
 export default {
-    UserController
+    UserController,
+    PostController
 }
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const multer = require('multer');
 const UserController = require("../controllers/user-controller");
-const authenticateToken = require('../middleware/auth')
+const authenticateToken = require('../middleware/auth');
+const PostController = require("../controllers/post-controller");
 
 const uploadDestination = 'uploads'
 
@@ -16,10 +17,17 @@ const storage = multer.diskStorage({
 
 const uploads = multer({ storage: storage })
 
+//Routes для пользователя
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 router.get("/current", authenticateToken, UserController.current);
 router.get("/users/:id", authenticateToken, UserController.getUserById);
 router.put("/users/:id", authenticateToken, UserController.updateUser);
+
+//Routes для постов
+router.post('/posts', authenticateToken, PostController.createPost)
+router.get('/posts', authenticateToken, PostController.getAllPost)
+router.get('/posts/:id', authenticateToken, PostController.getPostById)
+router.delete('/posts/:id', authenticateToken, PostController.deletePost)
 
 module.exports = router;
